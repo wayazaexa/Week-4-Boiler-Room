cf4eb9d5320000678548989497342519a75e66f8
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-10-07 09:04:41 +0200

    Added constructor, getter and setter.

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,38 @@
 package org.example;
 
 public class Customer {
+    private  String name;
+    private  String city;
+    private  int order_value;
+
+
+    public Customer(String name, String city, int order_value) {
+        this.name = name;
+        this.city = city;
+        this.order_value = order_value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getOrder_value() {
+        return order_value;
+    }
+
+    public void setOrder_value(int order_value) {
+        this.order_value = order_value;
+    }
 }
